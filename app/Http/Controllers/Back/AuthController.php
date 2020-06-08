ecb42f4f1a4f0a028fe279096d4b877f4de47ace
celal <celalcok@gmail.com>
2020-06-08 09:31:27 +0300

    websocket

--- app/Http/Controllers/Back/AuthController.php
+++ app/Http/Controllers/Back/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Admin;
 use App\Models\Config;
 use Illuminate\Support\Facades\Auth;
+
 class AuthController extends Controller
 {
     public function login(){
--- resources/views/back/layouts/header.blade.php
+++ resources/views/back/layouts/header.blade.php
@@ -8,9 +8,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
-  <title>@yield('title','Blog Sitesi Yönetim Paneli')</title>
-
+    <title>@yield('title','Blog Sitesi Yönetim Paneli')</title>
+  
   <!-- Custom fonts for this template-->
   <link href="{{asset('back/')}}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
